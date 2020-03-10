d50d6c3de125ae5c29ea610d591f6c4d3d75c788
BKLemonTea <jyf1128@naver.com>
2020-03-10 21:47:58 +0900

    1.6

--- manage.js
+++ manage.js
@@ -1,92 +1,97 @@
 const scriptName="manage.js";
 const VER = "ver 1.2";
-const MANAGER = "김재윤";
+const MANAGER = new Array(
+    "김재윤",
+    "허재녕"
+);
 
 function response(room, msg, sender, isGroupChat, replier, ImageDB, packageName, threadId){
-    //js 파일 목록
-    if(sender === MANAGER && msg === "/파일목록"){
-        var str="[ 파일 목록 ]";
-        for(var text of Api.getScriptNames()){
-            str += "\n" + text;
+    for(var man of MANAGER){
+        //js 파일 목록
+        if(sender === man && msg === "/파일목록"){
+            var str="[ 파일 목록 ]";
+            for(var text of Api.getScriptNames()){
+                str += "\n" + text;
+            }
+            replier.reply(str);
         }
-        replier.reply(str);
-    }
 
-    //리로드할 파일 목록 & 리로드
-    if(sender === MANAGER && msg.indexOf("/업데이트") !== -1 ){
-        if(msg.split(" ")[2] !== undefined){
-            if(msg.split(" ")[2].indexOf(".js") !== -1 ){
-                operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2] + ".js";
-            } else {
-                operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2];
+        //리로드할 파일 목록 & 리로드
+        if(sender === man && msg.indexOf("/업데이트") !== -1 ){
+            if(msg.split(" ")[2] !== undefined){
+                if(msg.split(" ")[2].indexOf(".js") !== -1 ){
+                    operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2] + ".js";
+                } else {
+                    operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2];
+                }
+                updateFile(operationJS);
+            }else{
+                if(msg.split(" ")[1].indexOf(".js") == -1 ){
+                    operationJS = msg.split(" ")[1] + ".js";
+                } else {
+                    operationJS = msg.split(" ")[1];
+                }
+                updateFile(operationJS);
             }
-            updateFile(operationJS);
-        }else{
-            if(msg.split(" ")[1].indexOf(".js") == -1 ){
-                operationJS = msg.split(" ")[1] + ".js";
-            } else {
-                operationJS = msg.split(" ")[1];
-            }
-            updateFile(operationJS);
         }
-    }
 
-    if(sender === MANAGER && msg.indexOf("/리로드") !== -1 ){
-        if(msg.split(" ")[2] !== undefined){
-            if(msg.split(" ")[2].indexOf(".js") !== -1 ){
-                operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2] + ".js";
-            } else {
-                operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2];
+        if(sender === man && msg.indexOf("/리로드") !== -1 ){
+            if(msg.split(" ")[2] !== undefined){
+                if(msg.split(" ")[2].indexOf(".js") !== -1 ){
+                    operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2] + ".js";
+                } else {
+                    operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2];
+                }
+                reloadFile(operationJS);
+            }else{
+                if(msg.split(" ")[1].indexOf(".js") == -1 ){
+                    operationJS = msg.split(" ")[1] + ".js";
+                } else {
+                    operationJS = msg.split(" ")[1];
+                }
+                reloadFile(operationJS);
             }
-            reloadFile(operationJS);
-        }else{
-            if(msg.split(" ")[1].indexOf(".js") == -1 ){
-                operationJS = msg.split(" ")[1] + ".js";
-            } else {
-                operationJS = msg.split(" ")[1];
-            }
-            reloadFile(operationJS);
         }
-    }
 
-    if(sender === MANAGER && msg.indexOf("/켜기") !== -1 ){
-        if(msg.split(" ")[2] !== undefined){
-            if(msg.split(" ")[2].indexOf(".js") !== -1 ){
-                operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2] + ".js";
-            } else {
-                operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2];
+        if(sender === man && msg.indexOf("/켜기") !== -1 ){
+            if(msg.split(" ")[2] !== undefined){
+                if(msg.split(" ")[2].indexOf(".js") !== -1 ){
+                    operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2] + ".js";
+                } else {
+                    operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2];
+                }
+                turnOn(operationJS);
+            }else{
+                if(msg.split(" ")[1].indexOf(".js") == -1 ){
+                    operationJS = msg.split(" ")[1] + ".js";
+                } else {
+                    operationJS = msg.split(" ")[1];
+                }
+                turnOn(operationJS);
             }
-            turnOn(operationJS);
-        }else{
-            if(msg.split(" ")[1].indexOf(".js") == -1 ){
-                operationJS = msg.split(" ")[1] + ".js";
-            } else {
-                operationJS = msg.split(" ")[1];
-            }
-            turnOn(operationJS);
         }
-    }
 
-    if(sender === MANAGER && msg.indexOf("/끄기") !== -1 ){
-        if(msg.split(" ")[2] !== undefined){
-            if(msg.split(" ")[2].indexOf(".js") !== -1 ){
-                operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2] + ".js";
-            } else {
-                operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2];
+        if(sender === man && msg.indexOf("/끄기") !== -1 ){
+            if(msg.split(" ")[2] !== undefined){
+                if(msg.split(" ")[2].indexOf(".js") !== -1 ){
+                    operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2] + ".js";
+                } else {
+                    operationJS = msg.split(" ")[1] + " " + msg.split(" ")[2];
+                }
+                turnOff(operationJS);
+            }else{
+                if(msg.split(" ")[1].indexOf(".js") == -1 ){
+                    operationJS = msg.split(" ")[1] + ".js";
+                } else {
+                    operationJS = msg.split(" ")[1];
+                }
+                turnOff(operationJS);
             }
-            turnOff(operationJS);
-        }else{
-            if(msg.split(" ")[1].indexOf(".js") == -1 ){
-                operationJS = msg.split(" ")[1] + ".js";
-            } else {
-                operationJS = msg.split(" ")[1];
-            }
-            turnOff(operationJS);
         }
-    }
 
-    if(sender === MANAGER && msg === "/도움말"){
-        replier.reply("[명령어] " + VER + "\n/파일목록\n/업데이트 test.js\n/리로드 test.js\n/켜기 test.js\n/끄기 test.js");
+        if(sender === man && msg === "/도움말"){
+            replier.reply("[명령어] " + VER + "\n/파일목록\n/업데이트 test.js\n/리로드 test.js\n/켜기 test.js\n/끄기 test.js");
+        }
     }
 
     function updateFile(file){
